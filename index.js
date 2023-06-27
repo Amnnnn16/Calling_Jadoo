7e103cc6856c89faaf20ace5322584e8b01b2a8d
Amnnnn16 <ag6835902@gmail.com>
2023-06-27 21:33:06 +0530

    basic functionality added

---
+++ index.js
@@ -0,0 +1,54 @@
+for(var i=0;i<document.querySelectorAll("button").length;i++){
+document.querySelectorAll("button")[i].addEventListener("click",function() {
+
+
+    var buttoninner=this.innerHTML;
+    play_sound(buttoninner);
+    when_pressed(buttoninner);
+    
+})}
+
+document.addEventListener("keypress",function(event){
+    play_sound(event.key);
+when_pressed(event.key);
+});
+
+function play_sound(key){
+    switch(key){
+        case "a":
+            var aud1=new Audio("audio/one.mp3");
+            aud1.play();
+            break;
+        
+        case "b":
+            var aud2=new Audio("audio/two.mp3");
+            aud2.play();
+            break;
+
+        case "c": 
+            var aud3=new Audio("audio/three.mp3");
+            aud3.play();
+            break;
+        
+        case "d":
+            var aud4=new Audio("audio/four.mp3");       
+            aud4.play();
+            break;
+        
+        case "e":
+            var aud5=new Audio("audio/five.mp3");
+            aud5.play();
+            break;
+        
+        default:
+            var aud1=new Audio("audio/one.mp3");
+            aud1.play();
+    }
+}
+function when_pressed(key){
+    document.querySelector("."+key).classList.add("pressed")
+    setTimeout(() => {
+        document.querySelector("."+key).classList.remove("pressed");
+        
+    }, 100);
+}
